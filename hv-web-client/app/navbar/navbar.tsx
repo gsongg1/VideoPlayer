9b6354036de8ef8b0b89ef818d7756d962c7b9af
George Song <georgesong97@gmail.com>
2024-07-31 17:40:03 -0600

    upload end-to-end complete

--- hv-web-client/app/navbar/navbar.tsx
+++ hv-web-client/app/navbar/navbar.tsx
@@ -7,27 +7,30 @@ import SignIn from "./sign-in";
 import { onAuthStateChangedHelper } from "../firebase/firebase";
 import { useEffect, useState } from "react";
 import { User } from "firebase/auth";
+import Upload from "./upload";
 
  //react server component
- export default function NavBar() {
-    // Initialize user state
-    const [user, setUser] = useState<User | null>(null);
-  
-    useEffect(() => {
-      const unsubscribe = onAuthStateChangedHelper((user) => {
-        setUser(user);
-      });
-  
-      // Cleanup subscription on unmount
-      return () => unsubscribe();
-    }, [] /* No dependencies, never rerun */);
-    return (
-        <nav className={styles.nav}>        
-            <Link href= "/">
-             <Image width = {90} height={20}
-                src= "/youtube-logo.svg" alt ="Youtube Logo"/>  
-         </Link>
-         <SignIn user={user}/>
-        </nav>
-    );
+ export default function Navbar() {
+  // Init user state
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  });
+
+  return (
+    <nav className={styles.nav}>
+      <Link href="/">
+        <Image width={90} height={20}
+          src="/youtube-logo.svg" alt="YouTube Logo"/>
+      </Link>
+      { user && <Upload /> }
+      <SignIn user={user} />
+    </nav>
+  );
 }
