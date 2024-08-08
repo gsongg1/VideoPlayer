197616ac900d018d996d2e777a36c849da382dd1
George Song <georgesong97@gmail.com>
2024-08-07 22:59:14 -0600

    getVideo

--- hv-web-client/app/firebase/functions.ts
+++ hv-web-client/app/firebase/functions.ts
@@ -3,11 +3,23 @@ import { getFunctions, httpsCallable } from 'firebase/functions';
 const functions = getFunctions();
 
 const generateUploadUrlFunction = httpsCallable(functions, 'generateUploadUrl');
+const getVideosFunction = httpsCallable(functions, 'getVideos');
+
+
+export interface Video {
+  id?: string,
+  uid?: string,
+  filename?: string,
+  status?: 'processing' | 'processed',
+  title?: string,
+  description?: string  
+}
 
 export async function uploadVideo(file: File) {
   const response: any = await generateUploadUrlFunction({
     fileExtension: file.name.split('.').pop()
   });
+
 
   // Upload the file to the signed URL
   const uploadResult = await fetch(response?.data?.url, {
@@ -20,3 +32,9 @@ export async function uploadVideo(file: File) {
 
   return uploadResult;
 }
+
+export async function getVideos() {
+  const response: any = await getVideosFunction();
+  return response.data as Video[];
+}
+
